b6530b78cfd2b9d5c769c48381a6525c75d24f64
Rodrigo Camacho <camachorod@gmail.com>
2017-02-05 02:01:37 +0100

    Changed the structure to include an amount function per movement

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+#Global Variables
+inflation = 0.02
+increase_life_level = 0.05  #this should be in function of my net income increase - maybe 50% of that increase
+increase_salary = 0.05
+
 class CapitalLocations:
     capitallocations_list =[]
     def __init__(self, name, amount, interest):
@@ -9,6 +14,8 @@ class CapitalLocations:
 #CapitalLocations defined by hand. Should be imported from JSON later.
 Livret_A = CapitalLocations("Livret_A", 12000, 0.02)
 Current = CapitalLocations("Current", 2950, 0)
+Bank = CapitalLocations("Bank", 0, 0)
+Market = CapitalLocations("Market", 0, 0)
 OUT = CapitalLocations("Out", 0, 0)
 
 class Movements:
@@ -17,36 +24,42 @@ class Movements:
         self.name = name
         self.from_location = from_location
         self.to_location = to_location
-        self.amount = amount #if negative, make negative
+        self.amount = amount
         self.periodicity = periodicity #defined in months
         self.movements_list.append(self)
-
-
-def CalculateInterest(capital, interest):
-    return capital*interest
-
-def ComplexMovements(from_location): 
-    if from_location == "Current" and Current.amount > 950 :
-        amount = Current.amount - 950
-    else:
-        amount = 0
-    return amount
-
+    
 #Movements define by hand. Should be imported from JSON later
 rent = Movements("Rent", "Current", "OUT", 900, 1)
 salary = Movements("Salary", "OUT", "Current", 2315.38, 1)
 food = Movements("Food", "Current", "OUT", 500, 1)
-Int_livret_a = Movements("LivA_interest", "OUT", "Livret_A", CalculateInterest(Livret_A.amount, Livret_A.interest), 12)
+Int_livret_a = Movements("LivA_interest", "OUT", "Livret_A", 0, 12)
 taxes = Movements("Taxes", "Current", "OUT", 3000, 12)
-current_to_livret_a = Movements("current_to_livret", "Current", "Livret_A", ComplexMovements("Current"),1)
+current_to_livret_a = Movements("current_to_livret", "Current", "Livret_A", 0, 1)
+
+#Insert taxes calculation function here to have accurate taxes.
+
+def calculate_movement_amounts(movement):
+    if movement == rent or movement == food:
+        movement.amount = movement.amount * (1+inflation)**(1/12) * (1+increase_life_level)**(1/12)
+    elif movement == salary:
+        movement.amount = movement.amount * (1+increase_salary)**(1/12)
+    elif movement == current_to_livret_a:
+        if Current.amount > 950: 
+            movement.amount = Current.amount - 950
+        else:
+            movement.amount = 0
+    elif movement == Int_livret_a:
+        movement.amount = movement.amount * (1 + Livret_A.interest)
+    #elif movement == taxes: 
+        #here you will call the taxes function with all the current amounts
 
 def Project(number_periods):
     for period in range(1,number_periods):
         for movement in Movements.movements_list:
-            if period%movement.periodicity == 0:
+            if period % movement.periodicity == 0:
+                calculate_movement_amounts(movement)
                 eval(movement.from_location).amount -= movement.amount
                 eval(movement.to_location).amount += movement.amount
-#Right now it's not working because the interest in livret a and removal amount from current to livret is only calculated once. I have to run the function in the loop to get something meanwhile
 Project(24)
 print Livret_A.amount
 print Current.amount
